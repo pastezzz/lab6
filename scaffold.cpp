30dae716f92e8cc3a1674f67228cdd8f5e7ed246
yutengz2 <yutengz2@stanford.edu>
2026-05-13 01:43:50 -0700

    Add random calculator commands

--- scaffold.cpp
+++ scaffold.cpp
@@ -1,27 +1,53 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <ctime>
+#include "randfuncs.h"
 
 using namespace std;
-
 
 int main(int argc, char *argv[]) {
     const string EXIT = "quit";
     string command;
+
+    srand(time(0));
+
     do {
-	cout << "calc: ";
+        cout << "calc: ";
         cin >> command;
 
-        if ("help" == command)
-        {
-            // to do
-	    cout << "help documentation" << endl;
+        if ("help" == command) {
+            cout << "Swiss Calculator Help" << endl;
+            cout << "Commands:" << endl;
+            cout << "  coin       flip a coin" << endl;
+            cout << "  d6         roll a six-sided die" << endl;
+            cout << "  d20        roll a twenty-sided die" << endl;
+            cout << "  quit       exit the calculator" << endl;
         }
-        else
-        {
-           // to do for start
-	   cout << "unknown command" << endl;
-	   return 1;
+        else if ("coin" == command) {
+            int result = flipCoin();
+
+            if (result == 0) {
+                cout << "heads" << endl;
+            }
+            else {
+                cout << "tails" << endl;
+            }
         }
+        else if ("d6" == command) {
+            cout << rollSixSidedDie() << endl;
+        }
+        else if ("d20" == command) {
+            cout << rollTwentySidedDie() << endl;
+        }
+        else if (EXIT == command) {
+            cout << "goodbye" << endl;
+        }
+        else {
+            cout << "unknown command" << endl;
+        }
+
     } while (EXIT != command);
+
     return 0;
 }
